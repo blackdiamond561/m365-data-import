3eae75628383140b14e1932e6367d9fbc5a7a04b
Sebastian Rogers <sebastian.rogers@beis.gov.uk>
2021-03-18 12:55:50 +0000

    Loads a single column

--- Load-Data.ps1
+++ Load-Data.ps1
@@ -21,24 +21,64 @@ param (
 )
 
 PROCESS {
+    $InformationPreference = "Continue"
+
+    function Invoke-CLI {
+        param(
+            # The command to execute
+            [Parameter(Mandatory)]
+            [string]
+            $Command,
+
+            # Output that overrides displaying the command, e.g. when it contains a plain text password
+            [string]
+            $Message = $Command
+        )
+
+        Write-Information -MessageData:$Message
+
+        # Az can output WARNINGS on STD_ERR which PowerShell interprets as Errors
+        $CurrentErrorActionPreference = $ErrorActionPreference
+        $ErrorActionPreference = "Continue" 
+
+        Invoke-Expression -Command:$Command
+        $ExitCode = $LastExitCode
+        Write-Information -MessageData:"Exit Code: $ExitCode"
+        $ErrorActionPreference = $CurrentErrorActionPreference
+
+        switch ($ExitCode) {
+            0 {
+                Write-Debug -Message:"Last exit code: $ExitCode"
+            }
+            default {
+                throw $ExitCode
+            }
+        }
+    }
+
     $SourceData | ForEach-Object {
         $Data = $PSItem
         $KeyValue = $Data.$Key
 
         $ListItemCollection = m365 spo listitem list --webUrl $WebUrl --title $ListTitle --camlQuery "<View><Query><Where><Eq><FieldRef Name='$Key' /><Value Type='Text'>$($Data.Title)</Value></Eq></Where></Query></View>" --output json | Join-String | ConvertFrom-Json -AsHashTable
 
+        $DataCollection = $Data.PSObject.Properties |
+            Foreach-Object {
+                Write-Output "--$($PSItem.Name) ""$($PSItem.Value)"""
+            }
+
         if ($ListItemCollection.length -eq 0) {
             if ($PSCmdlet.ShouldProcess("$ListTitle $KeyValue", "Add list item")) {
-                $ListItemCollection = @(m365 spo listitem add --webUrl $WebUrl --listTitle $ListTitle --Title "$($Data.Title)" --output json | Join-String | ConvertFrom-Json -AsHashTable)
+                $ListItemCollection = @(Invoke-CLI -Command:"m365 spo listitem add --webUrl $WebUrl --listTitle ""$ListTitle"" $DataCollection --output json" | Join-String | ConvertFrom-Json -AsHashTable)
             }
         }
 
         $ListItemCollection | Foreach-Object {
             $ListItem = $PSItem
             $ID = $ListItem.ID
+
             if ($PSCmdlet.ShouldProcess("$ListTitle $ID", "Update list item")) {
-                $ListItemUpdated = m365 spo listitem set --webUrl $WebUrl --listTitle $ListTitle --id $ID --Title $($Data.Title) --output json | Join-String | ConvertFrom-Json -AsHashTable
-                Write-Debug $ListItemUpdated
+                $ListItem = Invoke-CLI -Command "m365 spo listitem set --webUrl $WebUrl --listTitle ""$ListTitle"" --id $ID $DataCollection --output json" | Join-String | ConvertFrom-Json -AsHashTable
             }
         }
     }
