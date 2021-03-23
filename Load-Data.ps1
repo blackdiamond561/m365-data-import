3e42a0c13f7452d411fdca455899900b25ab9ff4
Sebastian Rogers <sebastian.rogers@beis.gov.uk>
2021-03-23 13:27:24 +0000

    Added script

--- Load-Data.ps1
+++ Load-Data.ps1
@@ -1,6 +1,6 @@
 [CmdletBinding(SupportsShouldProcess)]
 param (
-    # Data in the form of an array of hash tables, i.e. a CSV filed
+    # Data in the form of an array of hash tables, i.e. a CSV with keys thata re internal names of fields in the target list
     [Parameter(Mandatory, ValueFromPipeline)]
     $SourceData,
 
@@ -21,6 +21,7 @@ param (
 )
 
 PROCESS {
+    $ErrorActionPreference = "Stop"
     $InformationPreference = "Continue"
 
     function Invoke-CLI {
@@ -60,26 +61,29 @@ PROCESS {
         $Data = $PSItem
         $KeyValue = $Data.$Key
 
-        $ListItemCollection = m365 spo listitem list --webUrl $WebUrl --title $ListTitle --camlQuery "<View><Query><Where><Eq><FieldRef Name='$Key' /><Value Type='Text'>$($Data.Title)</Value></Eq></Where></Query></View>" --output json | Join-String | ConvertFrom-Json -AsHashTable
+        $ListItemCollection = m365 spo listitem list --webUrl "$WebUrl" --title $ListTitle --camlQuery "<View><Query><Where><Eq><FieldRef Name='$Key' /><Value Type='Text'>$KeyValue</Value></Eq></Where></Query></View>" --output json | Join-String | ConvertFrom-Json -AsHashTable
 
         $DataCollection = $Data.PSObject.Properties |
             Foreach-Object {
-                Write-Output "--$($PSItem.Name) ""$($PSItem.Value)"""
+                if ($PSItem.Value.length -gt 0) {
+                    Write-Output "--$($PSItem.Name) ""$($PSItem.Value.trim())"""
+                }
             }
 
         if ($ListItemCollection.length -eq 0) {
             if ($PSCmdlet.ShouldProcess("$ListTitle $KeyValue", "Add list item")) {
-                $ListItemCollection = @(Invoke-CLI -Command:"m365 spo listitem add --webUrl $WebUrl --listTitle ""$ListTitle"" $DataCollection --output json" | Join-String | ConvertFrom-Json -AsHashTable)
+                $ListItemCollection = @(Invoke-CLI -Command:"m365 spo listitem add --webUrl ""$WebUrl"" --listTitle ""$ListTitle"" $DataCollection --output json" | Join-String | ConvertFrom-Json -AsHashTable)
             }
         }
-
+        else {
         $ListItemCollection | Foreach-Object {
             $ListItem = $PSItem
             $ID = $ListItem.ID
 
             if ($PSCmdlet.ShouldProcess("$ListTitle $ID", "Update list item")) {
-                $ListItem = Invoke-CLI -Command "m365 spo listitem set --webUrl $WebUrl --listTitle ""$ListTitle"" --id $ID $DataCollection --output json" | Join-String | ConvertFrom-Json -AsHashTable
+                $ListItem = Invoke-CLI -Command "m365 spo listitem set --webUrl ""$WebUrl"" --listTitle ""$ListTitle"" --id $ID $DataCollection --output json" | Join-String | ConvertFrom-Json -AsHashTable
             }
+        }
         }
     }
 }
--- README.md
+++ README.md
@@ -9,13 +9,28 @@ Utilities to import data from file storage to m365 drives, e.g. SharePoint lists
 The scripts assume that the current identity is already authenticated by a suitable method and that that identity has suitable authorization on the source and target locations.
 
 ### Examples
-To upload data from a CSV file to a SharePoint list
 
-m365di --source ./source.csv --target lists/list
+To upload data from a CSV file to a SharePoint list ensuring all titles are unique
+
+```ps1
+m365 login
+Get-Content -Path:./data.csv |
+    ConvertFrom-Csv |
+    ./Load-Data.ps1 `
+        -Url:https://tenancy.sharepoint.com/sites/site `
+        -ListTitle:"My List" `
+        -Key:Title
+```
+
+CSV file uses one row per item and one field, with internal name, per field.
+
+```csv
+Title,Description
+My Title,"My Description"
+```
 
 ## Build
 
 ## Design
 
 ## Contributions
-
